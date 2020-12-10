dd90d959aec35ad7739a9311019f56cc8b9ad8bc
Joni Hämäläinen <snurppa@gmail.com>
2020-12-10 19:42:34 +0200

    d6p2

--- src/aoc2020/day6.clj
+++ src/aoc2020/day6.clj
@@ -1,14 +1,22 @@
 (ns aoc2020.day6
-  (:require [aoc2020.util :as u]))
+  (:require [aoc2020.util :as u]
+            [clojure.set :as set]))
 
 
-(defn group-answers [group]
+(defn anyone-group-answers [group]
   (set (reduce str group)))
+
+(defn everyone-group-answers [group]
+  (reduce set/intersection (map set group)))
 
 (defn part1 []
   (let [answers (u/day6-input)]
-    (->> (map group-answers answers)
+    (->> (map anyone-group-answers answers)
          (map count)
          (reduce + 0))))
 
-(defn part2 [])
+(defn part2 []
+  (let [answers (u/day6-input)]
+    (->> (map everyone-group-answers answers)
+         (map count)
+         (reduce + 0))))
